ea97d8669ae749d0ebb84c1e6b3cfcd9f37307fa
dj120599 <60100701+dj120599@users.noreply.github.com>
2020-02-03 11:34:39 +0000

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/FishyNote.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FishyNote : Form
+    {
+        bool MinMax = true;
+
+        public FishyNote()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(MinMax == true)
+            {
+                this.Size = new Size(478, 148);
+                MinMax = false;
+            }
+            else
+            {
+                this.Size = new Size(478, 450);
+                MinMax = true;
+            }
+         
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/FishyNotes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FishyNotes : Form
+    {
+        public FishyNotes()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNote_Click(object sender, EventArgs e)
+        {
+            FishyNote _newNote = new FishyNote();
+            if (_newNote != null)
+            {
+                _newNote.Show();
+            }
+          
+        }
+    }
+}
